249a3b5485d30c891823d2a46354ab77038b1c8e
Darshan K S <77616581+darshan-k-s@users.noreply.github.com>
2025-06-15 22:27:35 +1000

    Interpolation added

--- characters.m
+++ characters.m
@@ -1,4 +1,39 @@
 % Drawing ASCII
+
+function dense_stroke = densify_strokes(stroke, points_per_segment)
+    dense_stroke = [];
+    current_stroke = [];
+
+    for i = 1:size(stroke, 2) - 1
+        p1 = stroke(:, i);
+        p2 = stroke(:, i + 1);
+
+        if any(isnan(p1)) || any(isnan(p2))
+            % Add current stroke to full output with a NaN separator
+            if ~isempty(current_stroke)
+                dense_stroke = [dense_stroke, current_stroke, [NaN; NaN]];
+                current_stroke = [];
+            end
+            continue;
+        end
+
+        % Linear interpolation between p1 and p2
+        interp_x = linspace(p1(1), p2(1), points_per_segment);
+        interp_y = linspace(p1(2), p2(2), points_per_segment);
+        interp_segment = [interp_x; interp_y];
+
+        current_stroke = [current_stroke, interp_segment];
+    end
+
+    % Append last stroke
+    if ~isempty(current_stroke)
+        dense_stroke = [dense_stroke, current_stroke];
+    end
+end
+
+
+
+
 clear all; clc;
 
 % Start RVC module
@@ -15,7 +50,7 @@ scale = 0.04;
 
 
 % Movement params
-a = 0.8;
+a = 1.4;
 v = 0.5;
 r = 0.000;
 
@@ -30,20 +65,18 @@ start_pos = [-588.53, -350, z_lift, home(4:6)]; % Start pos of first stroke (mm)
 robo.movej(home);
 robo.movej(start_pos);
 
+str = '0123456789';
+
+
 text = hershey{'2'};
+points_per_segment = 10;  % Increase this to get more waypoints
+strokes = densify_strokes(text.stroke, points_per_segment);
 
-path = [scale*text.stroke; zeros(1,numcols(text.stroke))]; % create the path 
-
- %{
-disp(text.stroke);
-disp("----------------------------");
-disp([scale * text.stroke; zeros(1, numcols(text.stroke))]);
-disp("----------------------------");
- %}
+path = [scale*strokes; zeros(1,numcols(strokes))]; % create the path 
 
 % Where ever there is an nan it indicates that we need to lift up.
 k = find(isnan(path(1,:))); %Find index of NaN, i.e, column
-% disp(k);
+
 
 path(:,k) = path(:,k-1); path(3,k) = 0.2*scale; % Determine the hight of the lift up motions. 
 % 0.2 * scale is the height. 0.2 is in m
@@ -68,6 +101,9 @@ end
 
 % Execute the movement!
 poses = robo.movej(path);
+
+for i = 1:1000000
+end
 
 robo.drawPath(poses);
 
