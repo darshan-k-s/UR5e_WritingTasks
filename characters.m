0882825d0ea3c495a18287e0abcbfe3a0dc24400
Darshan K S <77616581+darshan-k-s@users.noreply.github.com>
2025-06-15 21:20:48 +1000

    Update characters.m

--- characters.m
+++ characters.m
@@ -1,28 +1,52 @@
-% Draw given character
+% Drawing ASCII
+clear all; clc;
 
-clear all; % Clear all runtime mem
-clc; % Clear the command window
+% Start RVC module
+startup_rvc;
+load hershey; % Load hershey fonts
+
+% Create RTDE instance
+host = '127.0.0.1';
+port = 30003;
+robo = rtde(host, port);
+
+% Writing params
+scale = 0.04;
 
 
-%% ----- Drawing Characters -----
-% Example of drawing a letter/digit from the rvc toolbox hershey library
+% Movement params
+a = 0.8;
+v = 0.5;
+r = 0.000;
 
-startup_rvc; % Startup the rvc toolbox
+% Considering length of pen
+z_write = 100;
+z_lift = 1.05 * z_write; % 0.5cm above writing pos
 
-load hershey; % Load in the hershey fonts
+home = [-588.53, -133.30, 227.00, 2.221, 2.221, 0.00];
+start_pos = [-588.53, -350, z_lift, home(4:6)]; % Start pos of first stroke (mm)
 
+% Move the robot to the home position
+robo.movej(home);
+robo.movej(start_pos);
 
-% Character to write
-character = hershey{'2'}; % Select the letter that you want to draw (Letter or number works)
-scale = 0.04; % Select the scale of the digit. 1 = 100%, 0.1 = 10% scale
+text = hershey{'2'};
 
-path = [scale*character.stroke; zeros(1,numcols(character.stroke))]; % create the path 
+path = [scale*text.stroke; zeros(1,numcols(text.stroke))]; % create the path 
+
+ %{
+disp(text.stroke);
+disp("----------------------------");
+disp([scale * text.stroke; zeros(1, numcols(text.stroke))]);
+disp("----------------------------");
+ %}
 
 % Where ever there is an nan it indicates that we need to lift up.
-k = find(isnan(path(1,:)));
+k = find(isnan(path(1,:))); %Find index of NaN, i.e, column
+% disp(k);
 
-% At these positions add in a z hight
-path(:,k) = path(:,k-1); path(3,k) = 0.2*scale; % Determine the hight of the lift up motions. 0.2 * scale is the height. 0.2 is in m
+path(:,k) = path(:,k-1); path(3,k) = 0.2*scale; % Determine the hight of the lift up motions. 
+% 0.2 * scale is the height. 0.2 is in m
 
 traj = [path'*1000]; % convert to the mm units so that we can use the rtde toolbox
 
@@ -30,52 +54,24 @@ traj = [path'*1000]; % convert to the mm units so that we can use the rtde toolb
 scatter3(traj(:,1), traj(:,2), traj(:,3));
 plot3(traj(:,1), traj(:,2), traj(:,3));
 
-
-
-%% Now use RTDE to actually draw the path
-
-% Creating RTDE obj and connecting to the UR socket
-host = '127.0.0.1'; % THIS IP ADDRESS MUST BE USED FOR THE VIRTUAL BOX VM
-% host = '192.168.0.100'; % THIS IP ADDRESS MUST BE USED FOR THE REAL ROBOT
-port = 30003;
-% Constructor of rtde to setup tcp connection
-rtde = rtde(host,port);
-
-
-% Setting home and starting from home
-home = [-588.53, -133.30, 227.00, 2.221, 2.221, 0.00];
-poses = rtde.movej(home);
 % Creating a path array
 path = [];
 
-%{
- The position of the first stroke should be located at the following x, y, z position:
- [-588.53, -350, 0]
-%}
-
-% setting move parameters
-v = 0.5;
-a = 1.2;
-blend = 0.001;
-z = 100;
-
-% Populate the path array
-for i = 1:length(traj)
-    disp(i);
-    disp(traj(i,1:3) + [-588.53, -133.30 100]);
-    point = [[(traj(i,1:3) + [-588.53, -133.30 z]),(home(4:6))],a,v,0,blend];
-    if isempty(path)
-        path = point;
-    else
-        path = cat(1,path,point);
-    end
+for i = 2:size(traj,1)
+    disp(traj(1, 1:3));
+    tcp_xyz = traj(i,1:3) + [start_pos(1), start_pos(2), z_write];
+    %tcp_xyz = traj(i,1:3) + [start_pos(1), start_pos(2), z_write] - traj(1, 1:3);  % or z_lift for pen-up
+    point = [tcp_xyz, home(4:6), a, v, 0, r];
+    path = [path; point];
 end
 
+
 % Execute the movement!
-poses = rtde.movej(path);
+poses = robo.movej(path);
 
-% Plot the path
-rtde.drawPath(poses);
+robo.drawPath(poses);
 
-% Closing the TCP Connection
-rtde.close();
+
+
+% Close RTDE instance
+robo.close();
